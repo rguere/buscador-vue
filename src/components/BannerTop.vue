6747bb56cae1c60edb2f19d0fbeac6d75a007689
isaac seijas <isaacseijas7@gmail.com>
2019-11-06 22:44:46 -0400

    anmacion btn Leer más...

--- src/components/BannerTop.vue
+++ src/components/BannerTop.vue
@@ -4,14 +4,21 @@
       <div>
         <h2>Buscador de Empresas</h2>
         <h5>Buscador de Empresas</h5>
-        <a href="" v-on:click.stop.prevent="show = !show">Leer más...</a>
+        <a href="" 
+          v-on:click.stop.prevent="show = !show"
+          v-scroll-to="(show)?{
+              el: '#infocif_leer_mas',
+              offset: -100,
+          }: {
+            el: '#app'
+          }">Leer más...</a>
       </div>
       <div>
         <p>¿Le gustaría poder acceder a la <b>información más completa y reciente de todas las empresas que operan en España</b> que sean de su interés?</p>
       </div>
     </div>
     <transition name="fade">
-      <div class="container-fluid" v-if="show">
+      <div class="container-fluid" v-if="show" id="infocif_leer_mas">
         <div class="flex-col-md-2">
           <div class="content">
             <p>Infocif, la Red Social de Empresas, lanza su nuevo buscador ofreciendo la posibilidad al usuario de obtener, de una manera rápida y sencilla, toda la información relevante sobre cada una de las empresas que operan en España que le resulten de su interés.</p>
