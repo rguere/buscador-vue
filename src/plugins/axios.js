e5a86efde1ec291d26cbb8d26d1b2beb62cf0354
isaac seijas <isaacseijas7@gmail.com>
2019-11-08 14:18:38 -0400

    axios.defaults.baseURL = 'http://18.212.252.233:5000/'

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = window.location.origin.concat('/')
+  axios.defaults.baseURL = 'http://18.212.252.233:5000/'
 } else {
   axios.defaults.baseURL = 'http://18.212.252.233:5000/'
 }
