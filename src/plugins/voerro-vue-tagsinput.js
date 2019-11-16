c815531fc4ac6c1305c815382959640c15fb5717
Isaac Seijas <isaacseijas7@gmail.com>
2019-11-16 01:32:52 -0400

    focus input in vue-tagsinput

--- src/plugins/voerro-vue-tagsinput.js
+++ src/plugins/voerro-vue-tagsinput.js
@@ -1,3 +1,11 @@
 import Vue from 'vue'
 import VoerroTagsInput from '@voerro/vue-tagsinput'
 Vue.component('tags-input', VoerroTagsInput)
+
+document.addEventListener('click', (event) => {
+	let target = event.target
+	if (target.classList.contains('tags-input')) {
+		let input = event.target.querySelector('input')
+		if (input) { input.focus() }
+	}
+})
