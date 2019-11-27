fe6a6c389aa57eddbfdef4bb26c287aac88d3a6b
Isaac Seijas <isaacseijas7@gmail.com>
2019-11-27 18:38:29 -0400

    epa

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -1,10 +1,10 @@
 import store from './../store/index'
-
+import toastr from 'toastr'
 export default async (to, from, next) => {
   if (!store.getters['auth/check'] && store.getters['auth/token']) {
     try {
       await store.dispatch('auth/fetchUser')
-    } catch (e) { console.error(e) }
+    } catch (e) { toastr.error('Error', 'Error') }
   }
 
   next()
