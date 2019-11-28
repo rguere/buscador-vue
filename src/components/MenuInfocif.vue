0d2abfece9145445d08be858b8652f20c600d68e
Isaac Seijas <isaacseijas7@gmail.com>
2019-11-28 10:01:26 -0400

    epa

--- src/components/MenuInfocif.vue
+++ src/components/MenuInfocif.vue
@@ -49,7 +49,7 @@
 
             <div class="hidden-xs hidden-sm">
               <li class="dropdown">
-                <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="download" aria-expanded="false"> <span class="glyphicon glyphicon-user"></span> {{ user.username }} <span class="caret"></span></a>
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="download" aria-expanded="false"> <span class="glyphicon glyphicon-user"></span> {{ (user.username)? user.username: 'user' }} <span class="caret"></span></a>
                 <ul class="dropdown-menu" aria-labelledby="download">
                   <li>
                     <a href="#" @click.prevent="logout">Cerrar sesión</a>
