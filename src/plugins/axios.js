bd0971a120091fe2465ce5a5436e98c4ef882cd5
isaacseijas7 <isaacseijas7@gmail.com>
2020-02-13 12:46:43 -0400

    http://dev.infocif.info/api

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,9 +4,9 @@ import swal from 'sweetalert2'
 import router from './../router'
 
 if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'http://api.infocif.es/api'
+  axios.defaults.baseURL = 'http://dev.infocif.info/api'
 } else {
-  axios.defaults.baseURL = 'http://api.infocif.es/api'
+  axios.defaults.baseURL = 'http://dev.infocif.info/api'
 }
 
 // Request interceptor
