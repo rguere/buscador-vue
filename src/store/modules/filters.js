4f5f90f32dea5b2317f4f6534d0db08cbcf94333
isaacseijas7 <isaacseijas7@gmail.com>
2020-04-06 09:44:40 -0400

    ajuste

--- src/store/modules/filters.js
+++ src/store/modules/filters.js
@@ -42,7 +42,7 @@ const initialState = () => {
       {
         name: "Número de empleados",
         slug: "filter_numero_de_empleados",
-        offset: 70,
+        offset: 180,
         apply: false,
         quantity: 0,
         key: 'empleados',
@@ -105,7 +105,7 @@ const initialState = () => {
       {
         name: "NIF",
         slug: "filter_nif",
-        offset: 140,
+        offset: 245,
         apply: false,
         quantity: 0,
         key: 'cif',
